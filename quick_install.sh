ed100c0ec9aa0a32e41460b4e7bc29f2fbd51aff
Othi <mnpq.raven@gmail.com>
2018-12-28 17:09:21 +0100

    small update

---
+++ quick_install.sh
@@ -0,0 +1,19 @@
+sudo pacman -S --noconfirm yay
+sudo pacman -S --noconfirm maim
+sudo pacman -S --noconfirm neofetch
+sudo pacman -S --noconfirm compton
+sudo pacman -S --noconfirm genymotion
+sudo pacman -S --noconfirm ranger
+sudo pacman -S --noconfirm arandr
+sudo pacman -S --noconfirm i3-wm
+sudo pacman -S --noconfirm mpd
+sudo pacman -S --noconfirm ncmpcpp
+sudo pacman -S --noconfirm copyq
+
+
+pip install --user powerline-status
+pip install --user git+git://github.com/powerline/powerline
+sudo pacman -S --noconfirm powerline-fonts
+
+git clone git://github.com/tobi-wan-kenobi/bumblebee-status
+mv othi-burgundy.json ~/bumblebee-status/
