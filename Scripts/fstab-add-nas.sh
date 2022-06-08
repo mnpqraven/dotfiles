20346117a0af42519771a55115ddc091d9399011
Othi <mnpq.raven@gmail.com>
2022-06-08 17:15:02 +0700

    Update fstab-add-nas.sh

--- Scripts/fstab-add-nas.sh
+++ Scripts/fstab-add-nas.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 # append the nas ip to fstab
 echo "//192.168.1.10/db1	/mnt/nas/db1	cifs		cache=loose,x-systemd.automount,credentials=/etc/.credentials 0 0" >> /etc/fstab
+#TEST THIS
+#cache=loose,noauto,x-systemd.device-timeout=5,_netdev
