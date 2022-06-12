2224b7c7b753fe403f6e8fa10b96ad56afa3360f
Othi <mnpq.raven@gmail.com>
2022-06-12 20:51:36 +0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -12,7 +12,7 @@ cd $HOME
 # INSTALLING STUFF
 # EDITOR
 sudo pacman -S --noconfirm neovim zsh-theme-powerlevel10k awesome-terminal-fonts ttf-font-awesome powerline-fonts
-sudo pacman -S --noconfirm texlive-most python-pip evince
+sudo pacman -S --noconfirm texlive-most texlive-langextra python-pip evince
 # MARKDOWN AND LATEX
 sudo pip3 install neovim-remote
 git config --global core.editor $EDITOR
