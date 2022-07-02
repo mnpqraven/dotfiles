b4b79ea03a5137dd04d5deb63d3f29c6a9d18fb1
Othi <mnpq.raven@gmail.com>
2022-07-02 11:44:01 +0700

    indenting, lsp documentation on K, comment plugin merge

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,25 +1,30 @@
+-- autopair SETUP
+require("nvim-autopairs").setup {}
+require('Comment').setup()
+
+-- LSP SETUP
 require("nvim-lsp-installer").setup {
-  automatic_installation = true
+    automatic_installation = true
 }
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lspconfig = require("lspconfig")
 
 lspconfig.sumneko_lua.setup {
-capabilities = capabilities
+    capabilities = capabilities
 }
 lspconfig.rust_analyzer.setup {
-capabilities = capabilities
+    capabilities = capabilities
 }
 lspconfig.volar.setup {
-capabilities = capabilities
+    capabilities = capabilities
 }
 lspconfig.tsserver.setup {
-capabilities = capabilities
+    capabilities = capabilities
 }
 lspconfig.eslint.setup {
-capabilities = capabilities
+    capabilities = capabilities
 }
-
-require('Comment').setup()
-require('nvim-autopairs').setup{}
+lspconfig.tailwindcss.setup {
+    capabilities = capabilities
+}
