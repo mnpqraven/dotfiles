30fb7b52fc516c6bce8f56826705e56946aa9b26
Othi <mnpq.raven@gmail.com>
2022-07-26 02:09:57 +0700

    polybar init

--- Scripts/nerd-fonts.sh
+++ Scripts/nerd-fonts.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 while true; do
-
-    read -p "Do you want to install nerd fonts ?(y/n)" yn
-    case $yn in
+    read -p "Do you want to install nerd fonts ?(y/n) " yn1
+    read -p "Do you want to install papirus-icon-theme ?(y/n) " yn2
+    case $yn1 in
         [yY] ) yay nerd-fonts-complete;
             exit;;
         [nN] ) echo "you can run this code again by running bash ~/dotfiles/Scripts/nerd-fonts.sh";
             exit;;
         * ) echo "invalid response";;
     esac
-
+    case $yn2 in
+        [yY] ) yay papirus-icon-theme-git;
+            exit;;
+        [nN] ) echo "you can run this code again by running bash ~/dotfiles/Scripts/nerd-fonts.sh";
+            exit;;
+        * ) echo "invalid response";;
+    esac
 done
 
 echo "you can run this code again by running bash ~/dotfiles/Scripts/nerd-fonts.sh"
