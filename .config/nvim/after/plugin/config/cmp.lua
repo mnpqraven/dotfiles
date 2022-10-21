e4d5fbf67b60007098cf9058b697417651231207
Othi <mnpq.raven@gmail.com>
2022-10-21 08:54:20 +0700

    lsp update for vim breaking changes hrsh7th/cmp-nvim-lsp#38

--- .config/nvim/after/plugin/config/cmp.lua
+++ .config/nvim/after/plugin/config/cmp.lua
@@ -79,5 +79,5 @@ vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 vim.keymap.set('n', '<space>qf', vim.lsp.buf.code_action, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 
--- .config/nvim/after/plugin/config/lsp.lua
+++ .config/nvim/after/plugin/config/lsp.lua
@@ -1,51 +1,52 @@
 -- LSP SETUP
-require("nvim-lsp-installer").setup {
-  automatic_installation = true
-}
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "sumneko_lua", "rust_analyzer", "taplo", "bashls",
+    "volar", "tsserver", "eslint", "jsonls",
+    "texlab", "omnisharp", "tailwindcss",
+  }
+})
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require("lspconfig")
-
 lspconfig.sumneko_lua.setup {
   capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
     },
   },
 }
 -- config debugging later
-require('rust-tools').setup {}
+local rt = require('rust-tools')
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+    end
+  }
+})
+lspconfig.taplo.setup { capabilities = capabilities }
 
-lspconfig.volar.setup {
-  capabilities = capabilities
-}
-lspconfig.tsserver.setup {
-  capabilities = capabilities
-}
-lspconfig.eslint.setup {
-  capabilities = capabilities
-}
-lspconfig.tailwindcss.setup {
-  capabilities = capabilities,
-  on_attach = function (_, bufnr)
+lspconfig.volar.setup { capabilities = capabilities }
+lspconfig.tsserver.setup { capabilities = capabilities }
+lspconfig.eslint.setup { capabilities = capabilities }
+lspconfig.jsonls.setup { capabilities = capabilities }
+
+lspconfig.bashls.setup { capabilities = capabilities }
+lspconfig.texlab.setup { capabilities = capabilities }
+lspconfig.omnisharp.setup { capabilities = capabilities }
+lspconfig.tailwindcss.setup { capabilities = capabilities,
+  on_attach = function(_, bufnr)
     require("tailwindcss-colors").buf_attach(bufnr)
   end
 }
-lspconfig.bashls.setup {
-  capabilities = capabilities
-}
-lspconfig.taplo.setup {
-  capabilities = capabilities
-}
-lspconfig.texlab.setup {
-  capabilities = capabilities
-}
 
-vim.diagnostic.config({ update_in_insert = true, virtual_text = false, severity_sort = true})
+vim.diagnostic.config({ update_in_insert = true, virtual_text = false, severity_sort = true })
 
 -- signs on gutter columns
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
