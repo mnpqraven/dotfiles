60f81c3259495d5b528f6349f9ce72fd434b2520
Othi <mnpq.raven@gmail.com>
2022-12-20 17:05:43 +0700

    tabstop for tsx

--- .config/nvim/lua/othi/settings.lua
+++ .config/nvim/lua/othi/settings.lua
@@ -58,6 +58,12 @@ vim.api.nvim_create_autocmd(
 vim.opt.tabstop        = 4
 vim.opt.shiftwidth     = 4
 end })
+vim.api.nvim_create_autocmd(
+{ "BufRead", "BufNewFile" },
+{ pattern = { "*.tsx", "*.ts" }, callback = function ()
+vim.opt.tabstop        = 2
+vim.opt.shiftwidth     = 2
+end })
 
 vim.cmd([[
 "set path+=**
