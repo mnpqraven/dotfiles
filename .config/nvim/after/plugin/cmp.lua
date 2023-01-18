d96f21095e5ceec56a08641f0bd7fa52683440f3
Othi <mnpq.raven@gmail.com>
2023-01-19 00:42:41 +0700

    forgot cmp file

---
+++ .config/nvim/after/plugin/cmp.lua
@@ -0,0 +1,66 @@
+local lsp = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local lspkind = require('lspkind')
+
+local cmp_configs = lsp.defaults.cmp_config({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp', keyword_length = 1 },
+    { name = 'luasnip', keyword_length = 1 },
+    { name = 'crates' },
+  }, {
+    { name = 'buffer' },
+  }
+  ),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50,
+      ellipsis_char = '...',
+      menu = {}
+    })
+  }
+})
+
+cmp.event:on(
+  'confirm_done', cmp_autopairs.on_confirm_done())
+
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+cmp.setup.filetype('norg', {
+  sources = cmp.config.sources({
+    { name = 'neorg' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+cmp.setup(cmp_configs)
